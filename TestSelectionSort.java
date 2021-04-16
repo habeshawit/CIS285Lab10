47d79ae698e35bc1089fab2d8ec3afe16cd3127b
Shalom <shalomsk@umich.edu>
2021-04-15 21:26:53 -0400

    Mixed and Duplicate array tests passing

--- TestSelectionSort.java
+++ TestSelectionSort.java
@@ -40,10 +40,52 @@ public class testSelectionSort {
     public void testMixed(){
 
         /** Test data contains with both positive, negative and zeros **/
+				int[] arr = new int[5];
+        arr[0] = 0;
+        arr[1] = -8;
+        arr[2] = 7;
+        arr[3] = -10;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -8;
+        Sortedarr[2] = 0;
+        Sortedarr[3] = 2;
+        Sortedarr[4] = 7;
+
+        SelectionSort temp1 = new SelectionSort();
+        int[] newArr = temp1.basicSelectionSort(arr);
+
+        for (int i = 0; i < arr.length; i++)
+        {
+           assertEquals("Same element", newArr[i], Sortedarr[i]);
+        }
     }
 
     public void testDuplicates(){
 
         /** Test data contains duplicates **/
+				int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -8;
+        arr[2] = 7;
+        arr[3] = -10;
+        arr[4] = 7;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -8;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = 7;
+        Sortedarr[4] = 7;
+
+        SelectionSort temp1 = new SelectionSort();
+        int[] newArr = temp1.basicSelectionSort(arr);
+
+        for (int i = 0; i < arr.length; i++)
+        {
+           assertEquals("Same element", newArr[i], Sortedarr[i]);
+        }
     }
 }
