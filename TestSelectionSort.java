52596ca60510b8624354ea5ba2225f3201f3de7c
Shalom <shalomsk@umich.edu>
2021-04-15 21:24:53 -0400

    Pos and Neg tests passing

--- TestSelectionSort.java
+++ TestSelectionSort.java
@@ -30,11 +30,39 @@ public class testSelectionSort {
         Sortedarr[4] = 10;
 
         /** add tests to check for this unit test **/
+        SelectionSort temp1 = new SelectionSort();
+        int[] newArr = temp1.basicSelectionSort(arr);
+
+        for (int i = 0; i < arr.length; i++)
+        {
+           assertEquals("Same element", newArr[i], Sortedarr[i]);
+        }
     }
 
     public void testNegative(){
 
         /** Test data contains negative values only **/
+        int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        SelectionSort temp1 = new SelectionSort();
+        int[] newArr = temp1.basicSelectionSort(arr);
+
+        for (int i = 0; i < arr.length; i++)
+        {
+           assertEquals("Same element", newArr[i], Sortedarr[i]);
+        }
     }
 
     public void testMixed(){
